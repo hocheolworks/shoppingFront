ee1bd41f0f1dfc6bdbe958a168d4090051fecfd2
Hyeongwook Kim <jahanda@naver.com>
2022-04-30 22:41:23 +0900

    redux 유지

--- src/store.tsx
+++ src/store.tsx
@@ -1,7 +1,19 @@
-import {createStore, applyMiddleware} from "redux";
+import {createStore, applyMiddleware, compose} from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./redux/reducers/root-reducer";
+import promiseMiddleware from 'redux-promise';
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+// const store = createStore(rootReducer, applyMiddleware(thunk));
+const persistConfig = {
+  key: 'root',
+  storage
+};
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const persisted = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persisted, compose(
+  applyMiddleware(promiseMiddleware, thunk)
+));
 
 export default store;
