5d4ac9cf0178677212907b90469dc0f0e722f551
Hyeongwook Kim <jahanda@naver.com>
2022-06-16 00:34:42 +0900

    footer 진행 중, 약관 아직 안함

--- src/component/Footer/Footer.tsx
+++ src/component/Footer/Footer.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { FC } from "react";
 
 // import "./Footer.css";
@@ -9,14 +10,20 @@ const Footer: FC = () => {
         <div className="justify-content-between">
           <div className="footer-container">
             <div className="footer-item">
-              <h3>진솔유통</h3>
-              <p>010-4826-0519</p>
+              <p>진솔유통 - 마트가방</p>
+              <p>대표 : 김진솔 | 사업자번호 : 206-34-61779</p>
+              <p>본사 : 서울시 용산구 신흥로 3길 22-1</p>
+              <p>영종도 공장 : 인천시 중구 운북동 168-29</p>
+              <p>전화 : 010-2269-9422 또는 010-4826-0519</p>
+              <p>통신판매신고 : Copyright (C) 2022 jinsoltrade all rights reserved.</p>
             </div>
             <div className="footer-item">
-              <p>상담 가능 시간 : (평일) 09:00 - 20:00</p>
+              <p>상담 가능 시간 : 24시간 전화 상담 가능</p>
             </div>
-            <div className="footer-item">
-              <p>© Copy right 진솔유통</p>
+            <div>
+              <h6>진솔유통 기본약관</h6>
+              <Link href={'/'}><a className="mr-5">개인정보 처리방침</a></Link>
+              <Link href={'/'}><a className="mr-5">전자금융거래 기본약관</a></Link>
             </div>
           </div>
         </div>
